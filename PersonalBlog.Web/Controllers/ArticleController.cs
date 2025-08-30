365dbc2fec1dbced40d5c56bb9f2a9b3cea13548
Valeriia <valeriia.vp1@yandex.ru>
2025-08-30 10:17:21 +0300

    Added new controllers

---
+++ PersonalBlog.Web/Controllers/ArticleController.cs
@@ -0,0 +1,10 @@
+namespace PersonalBlog.Controllers;
+
+public class ArticleController
+{
+    /*создание,
+     редактирование,
+     удаление статьи,
+     получение статьи по айди,
+     получение всех статей по автору*/
+}
---
+++ PersonalBlog.Web/Controllers/CommentController.cs
@@ -0,0 +1,10 @@
+namespace PersonalBlog.Controllers;
+
+public class CommentController
+{
+    /*создание,
+    редактирование,
+    удаление комментария,
+    получение комментария по айди,
+    получение всех комментариев*/
+}
